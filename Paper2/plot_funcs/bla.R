e74bd2e3c1c52290bbf43eecf49ee31ab9486877
Dustin Tracy <dtracy.uf@gmail.com>
2020-05-12 14:50:49 -0400

    committing for adrian

--- Paper2/plot_funcs/bla.R
+++ Paper2/plot_funcs/bla.R
@@ -19,9 +19,9 @@ plot_blas <- function(bond_data){
   bond_data %>%
     filter(t < 1) %>% 
     ggplot(aes(x = t, y=BLA, color = State)) +
-    geom_line() +
+    geom_line(alpha = 0.5) +
     geom_smooth(method = 'gam', formula = y ~ s(x, bs = "cs")) +
-    labs(x = "Time (fs)") +
+    labs(x = "Time (ps)") +
     theme_bw() +
     theme(axis.text=element_text(size=20),
           axis.title=element_text(size=20),
@@ -57,3 +57,44 @@ bond_data <- bind_rows(
   sm_ppv3_vac
 )
 plot_blas(bond_data = bond_data)
+
+######################
+# PPV3 - NO2
+######################
+timestep = 5 #fs
+# S0 & S1 - Vacuum from ~/backup1/TestRuns/Paper1/ppvno2-vacuum
+# Near to NO2 Bonds: (6, 7), (7, 8), (8, 9)
+# Far from NO2 Bonds: (17, 16) (16, 15), (15, 14)
+s0_ppv3_no2_vac <- collect_blas(runtime = 10,
+                                timestep = timestep,
+                                infile = 'bla-ppv3-no2-s0.csv',
+                                state = 'S0') %>% 
+  mutate(t = t - 4) %>% 
+  filter(t > 0)
+s1_ppv3_no2_vac_near <- collect_blas(runtime = 10,
+                                timestep = timestep,
+                                infile = 'bla-ppv3-no2-s1-1.csv',
+                                state = 'S1-near')
+s1_ppv3_no2_vac_far <- collect_blas(runtime = 10,
+                                     timestep = timestep,
+                                     infile = 'bla-ppv3-no2-s1-2.csv',
+                                     state = 'S1-far')
+# Sm - Vacuum from ~/backup4/TestRuns/Paper2/ppv-no2-validation/vacuum-bla
+# Bonds same
+sm_ppv3_no2_vac_near <- collect_blas(runtime = 1,
+                                     timestep = timestep,
+                                     infile = 'bla-ppv3-no2-sm-1.csv',
+                                     state = 'Sm-near')
+sm_ppv3_no2_vac_far <- collect_blas(runtime = 1,
+                                    timestep = timestep,
+                                    infile = 'bla-ppv3-no2-sm-2.csv',
+                                    state = 'Sm-far')
+
+bond_data <- bind_rows(
+  s0_ppv3_no2_vac,
+  s1_ppv3_no2_vac_near,
+  s1_ppv3_no2_vac_far,
+  sm_ppv3_no2_vac_near,
+  sm_ppv3_no2_vac_far
+)
+plot_blas(bond_data)
