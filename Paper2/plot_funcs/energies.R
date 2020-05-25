7f682e8de52d3f9ad8554cc0f24a2ed0c7b4afbc
Dustin Tracy <dtracy.uf@gmail.com>
2020-05-25 13:23:00 -0400

    About to wrap potential energies

--- Paper2/plot_funcs/energies.R
+++ Paper2/plot_funcs/energies.R
@@ -7,10 +7,6 @@ data_mean <- data %>% group_by(`Time-fs`, State) %>%
   summarise(MeanEnergy = mean(`Energy-eV`)) %>% 
   mutate(State = as.factor(State))
 
-data_mean %>% 
-  ggplot(aes(x = `Time-fs`, y = MeanEnergy, group=State, color=State)) +
-  geom_line()
-
 #
 # Reading Potential Energies
 #
@@ -21,27 +17,41 @@ read_potential <- function (filein, system_name){
     mutate(System = system_name)
 }
 
-plot_potentials <- function (potentials){
+plot_potentials <- function (potentials, legend_breaks, legend_labels){
   potentials %>% 
     ggplot(aes(x = `Time-fs`, y = MeanEnergy, color = System)) +
-    geom_point() +
+    geom_point(size = 0.5) +
     labs(x="Time (fs)", y="Energy (eV)")+
+    scale_color_discrete(breaks = legend_breaks, labels = legend_labels) +
     theme_bw() +
     theme(axis.text=element_text(size=20),
           axis.title=element_text(size=20),
-          legend.title = element_text(size = 15),
-          legend.text = element_text(size = 15)
+          legend.title = element_blank(),
+          legend.text = element_text(size = 15),
+          legend.text.align = 0
     )
 }
 
-plot(1:10, xlab=expression('hi'[5]*'there'[6]^8*'you'[2]))
 #
 # PPV3
 #
 pot_ppv3_no_trivial <- read_potential('ppv3/potential-no-trivial.csv', 'Vacuum (No Trivial)')
 pot_ppv3_vacuum <- read_potential('ppv3/potential-vacuum.csv','Vacuum')
-pot_ppv3_ch3oh <- read_potential('ppv3/potential-ch3oh.csv', expression('CH3OH'))
-pot_ppv3_ch3oh_5s <- read_potential('ppv3/potential-ch3oh-5s.csv', 'CH3OH-5s')
+pot_ppv3_ch3oh <- read_potential('ppv3/potential-ch3oh.csv', 'Methanol')
+pot_ppv3_ch3oh_5s <- read_potential('ppv3/potential-ch3oh-5s.csv', 'Methanol-5s')
+pot_ppv3_vacuum_s0 <- read_csv('ppv3/potential-s0-vacuum.csv') %>% 
+  filter(State == 0) %>% 
+  group_by(`Time-fs`) %>% 
+  summarise(MeanEnergy = mean(`Potential-eV`)) %>% 
+  filter(`Time-fs` >= 19500) %>% mutate(`Time-fs` = `Time-fs` - 19500) %>% 
+  mutate(System = 'S0 Vacuum')
+pot_ppv3_vacuum_s1 <- read_csv('ppv3/potential-s1-vacuum.csv') %>% 
+  filter(State == 1) %>% 
+  group_by(`Time-fs`) %>% 
+  summarise(MeanEnergy = mean(`Potential-eV`)) %>% 
+  filter(`Time-fs` >= 500) %>% mutate(`Time-fs` = `Time-fs` - 500) %>% 
+  mutate(System = 'S1 Vacuum')
+  
 # This energy is significanly lower because of the additional ch3oh atoms in the
 # qm calculation. For comparison, we are going to adjust the values mean energy
 # values by the initial value of the solute in mm
@@ -50,16 +60,52 @@ pot_ppv3_ch3oh_5s <- pot_ppv3_ch3oh_5s %>% mutate(MeanEnergy = MeanEnergy - adju
 
 potentials <- bind_rows(pot_ppv3_no_trivial,
                         pot_ppv3_vacuum,
+                        pot_ppv3_vacuum_s0,
+                        pot_ppv3_vacuum_s1,
                         pot_ppv3_ch3oh,
                         pot_ppv3_ch3oh_5s)
-plot_potentials(potentials)
+
+legend_breaks <- c("Vacuum (No Trivial)",
+                   "Vacuum",
+                   "S0 Vacuum",
+                   "S1 Vacuum",
+                   "Methanol",
+                   "Methanol-5s")
+legend_labels <- c(expression(paste(S[m], "Vacuum NT")),
+                   expression(paste(S[m], "Vacuum")),
+                   expression(paste(S[0], " Vacuum")),
+                   expression(paste(S[1], " Vacuum")),
+                   expression(paste(S[m], " Methanol")),
+                   expression(paste(S[m], " Methanol-5s")))
+
+plot_potentials(potentials, legend_breaks, legend_labels)
 
 #
 # PPV3-NO2
 #
 pot_ppv3_no2_vacuum <- read_potential('ppv3_no2/potential-vacuum.csv','Vacuum')
-pot_ppv3_no2_ch3oh <- read_potential('ppv3_no2/potential-ch3oh.csv', 'CH3OH')
-pot_ppv3_no2_ch3oh_5s <- read_potential('ppv3_no2/potential-ch3oh-5s.csv', 'CH3OH-5s')
+pot_ppv3_no2_ch3oh <- read_potential('ppv3_no2/potential-ch3oh.csv', 'Methanol')
+pot_ppv3_no2_ch3oh_5s <- read_potential('ppv3_no2/potential-ch3oh-5s.csv', 'Methanol-5s')
+# The following are taken from 
+pot_ppv3_no2_vacuum_s0 <- read_csv('ppv3_no2/potential-s0-vacuum.csv') %>% 
+  filter(State == 0) %>% 
+  group_by(`Time-fs`) %>% 
+  summarise(MeanEnergy = mean(`Potential-eV`)) %>% 
+  filter(`Time-fs` >= 9500) %>% mutate(`Time-fs` = `Time-fs` - 9500) %>% 
+  mutate(System = 'S0 Vacuum')
+pot_ppv3_no2_vacuum_s1 <- read_csv('ppv3_no2/potential-s1-vacuum.csv') %>% 
+  filter(State == 1) %>% 
+  group_by(`Time-fs`) %>% 
+  summarise(MeanEnergy = mean(`Potential-eV`)) %>% 
+  filter(`Time-fs` >= 9500) %>% mutate(`Time-fs` = `Time-fs` - 9500) %>% 
+  mutate(System = 'S1 Vacuum')
+pot_ppv3_no2_ch3oh_s1 <- read_csv('ppv3_no2/potential-s1-methanol.csv') %>% 
+  filter(State == 1) %>% 
+  group_by(`Time-fs`) %>% 
+  summarise(MeanEnergy = mean(`Potential-eV`)) %>% 
+  filter(`Time-fs` >= 9500) %>% mutate(`Time-fs` = `Time-fs` - 9500) %>% 
+  mutate(System = 'S1 Methanol')
+
 # This energy is significanly lower because of the additional ch3oh atoms in the
 # qm calculation. For comparison, we are going to adjust the values mean energy
 # values by the initial value of the solute in mm
@@ -67,6 +113,25 @@ adjust_factor <- pot_ppv3_no2_ch3oh_5s[[1,2]] - pot_ppv3_no2_ch3oh[[1,2]]
 pot_ppv3_no2_ch3oh_5s <- pot_ppv3_no2_ch3oh_5s %>% mutate(MeanEnergy = MeanEnergy - adjust_factor)
 
 potentials <- bind_rows(pot_ppv3_no2_vacuum,
+                        pot_ppv3_no2_vacuum_s0,
+                        pot_ppv3_no2_vacuum_s1,
                         pot_ppv3_no2_ch3oh,
+                        pot_ppv3_no2_ch3oh_s1,
                         pot_ppv3_no2_ch3oh_5s)
-plot_potentials(potentials)
+
+legend_breaks <- c("Vacuum",
+                   "S0 Vacuum",
+                   "S1 Vacuum",
+                   "Methanol",
+                   "S1 Methanol",
+                   "Methanol-5s")
+legend_labels <- c(expression(paste(S[m], " Vacuum")),
+                   expression(paste(S[0], " Vacuum")),
+                   expression(paste(S[1], " Vacuum")),    
+                   expression(paste(S[m], " Methanol")),
+                   expression(paste(S[1], " Methanol")),
+                   expression(paste(S[m], " Methanol-5s")))
+
+plot_potentials(potentials, legend_breaks, legend_labels)
+
+
--- Paper2/plot_funcs/regression.R
+++ Paper2/plot_funcs/regression.R
@@ -37,6 +37,12 @@ fit_s1 <- function (df) {
   s1_v_time %>% mutate(Fitted = f(t)) %>% gather("Type", "Pop", 3:4)
 }
 
+##################################
+# PPV3
+##################################
+tammie_vacuum <- as_tibble(import("ppv3/tammie_reproduction/512-no-trivial/all_pops.txt"))
+colnames(tammie_vacuum) <- get_statenames(tammie_vacuum)
+tammie_vacuum <- tidy(tammie_vacuum, "Vacuum NT")
 
 vacuum <- as_tibble(import("ppv3/vacuum/all_pops.txt"))
 colnames(vacuum) <- get_statenames(vacuum)
@@ -44,41 +50,93 @@ vacuum <- tidy(vacuum, 'Vacuum')
 
 ch3oh <- as_tibble(import("ppv3/ch3oh/all_pops.txt"))
 colnames(ch3oh) <- get_statenames(ch3oh)
-ch3oh <- tidy(ch3oh, 'CH3OH')
+ch3oh <- tidy(ch3oh, 'Methanol')
 
 ch3oh_5s <- as_tibble(import("ppv3/ch3oh_5s/all_pops.txt"))
 colnames(ch3oh_5s) <- get_statenames(ch3oh_5s)
-ch3oh_5s <- tidy(ch3oh_5s, 'CH3OH 5QM')
+ch3oh_5s <- tidy(ch3oh_5s, 'Methanol-5s')
 
-s1_v_time <- map(list(vacuum, ch3oh, ch3oh_5s), fit_s1) %>%
-  bind_rows()
+s1_v_time_ppv3 <- map(list(tammie_vacuum, vacuum, ch3oh, ch3oh_5s), fit_s1) %>%
+  bind_rows() %>% mutate(Solute = 'PPV3')
+
+vacuum_ppv3 <- vacuum %>% 
+  mutate(Solute = 'PPV3')
+
+##################################
+# PPV3-NO2
+##################################
+vacuum <- as_tibble(import("ppv3-no2/vacuum/all_pops.txt"))
+colnames(vacuum) <- get_statenames(vacuum)
+vacuum <- tidy(vacuum, 'Vacuum')
+
+ch3oh <- as_tibble(import("ppv3-no2/ch3oh/all_pops.txt"))
+colnames(ch3oh) <- get_statenames(ch3oh)
+ch3oh <- tidy(ch3oh, 'Methanol')
+
+ch3oh_5s <- as_tibble(import("ppv3-no2/ch3oh_5s/all_pops.txt"))
+colnames(ch3oh_5s) <- get_statenames(ch3oh_5s)
+ch3oh_5s <- tidy(ch3oh_5s, 'Methanol-5s')
+
+s1_v_time_ppv3_no2 <- map(list(vacuum, ch3oh, ch3oh_5s), fit_s1) %>%
+  bind_rows() %>% mutate(Solute = 'PPV3-NO2')
+
+vacuum_ppv3_no2 <- vacuum %>% 
+  mutate(Solute = 'PPV3-NO2')
+
+##################################
+# Plot S1
+##################################
+
+s1_v_time <- bind_rows(
+  s1_v_time_ppv3,
+  s1_v_time_ppv3_no2
+)
 
 graph_title = expression(paste(PPV[3]))
 graph_caption = expression(paste(S[1], "Population"))
 
-s1_v_time %>% filter(Type == "Measured") %>%  ggplot(aes(x=t, y=Pop)) +
+s1_v_time %>% filter(Type == "Measured") %>% 
+  filter(t <= 500) %>% 
+  ggplot(aes(x=t, y=Pop)) +
   coord_cartesian(ylim = c(0,1))+
   labs(x="Time (fs)", y="Population")+
   geom_line(mapping = aes(color = Solvent), size=1)+
+  facet_wrap(~ Solute) +
   theme_bw() +
   theme(axis.text=element_text(size=20),
         axis.title=element_text(size=20),
-        legend.title = element_text(size = 15),
-        legend.text = element_text(size = 15)
-        )
-
-all_pops <- vacuum %>% filter(State %in% c('Sm', 'S7', 'S2', 'S1'))
-vacuum %>% ggplot(aes(x=t, y=Measured)) +
-  geom_line(mapping = aes(color = State), size = 1) +
-  labs(x="Time (fs)", y="Population")+
-  theme_bw() +
-  theme(axis.text=element_text(size=20),
-        axis.title=element_text(size=20),
+        legend.position = "top",
         legend.title = element_text(size = 15),
         legend.text = element_text(size = 15)
   )
 
-# Plot the fitted tenua data for ppv3
+###################################
+# Plot all pops in vacuum
+###################################
+vacuum <- bind_rows(
+  vacuum_ppv3 %>% filter(State %in% c('Sm', 'S7', 'S2', 'S1')),
+  vacuum_ppv3_no2 %>% filter(State %in% c('Sm', 'S9', 'S2', 'S1'))
+)
+
+all_pops <- vacuum %>% 
+  filter(t <= 500)
+all_pops %>% ggplot(aes(x=t, y=Measured)) +
+  geom_line(mapping = aes(color = State), size = 1) +
+  labs(x="Time (fs)", y="Population")+
+  facet_wrap(~ Solute) +
+  theme_bw() +
+  theme(axis.text=element_text(size=20),
+        axis.title=element_text(size=20),
+        legend.title = element_text(size = 15),
+        legend.text = element_text(size = 15),
+        legend.position = "top"
+  )
+
+
+
+###################################
+# Plot fitted tenua data
+###################################
 vacuum <- as_tibble(read_table("ppv3/vacuum/ppv3_fitted_sm.txt", col_names = FALSE))
 state_names <- c('t', 'Sm Fitted', 'S1 Fitted', 'S2 Fitted', 'SL Fitted', 'S7 Fitted', 'S8 Fitted', 'Sm', 'S1', 'S2', 'SL', 'S7', 'S8')
 colnames(vacuum) <-state_names
